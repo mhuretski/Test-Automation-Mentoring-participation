74bc24b5f2f6172e45aaf658802f2086c0d91348
Maksim Huretski <Maksim_Huretski@epam.com>
2018-07-21 03:27:18 +0300

    useless stuff deleton

--- PreSelectionTasks/src/TaskTwo/TaskTwoFlowers/Main.java
+++
@@ -1,9 +0,0 @@
-package TaskTwo.TaskTwoFlowers;
-
-class Main {
-    public static void main(String[] args) {
-
-
-
-    }
-}
--- PreSelectionTasks/src/TaskTwo/TaskTwoSpeedComparison/Main.java
+++
@@ -1,9 +0,0 @@
-package TaskTwo.TaskTwoSpeedComparison;
-
-class Main {
-    public static void main(String[] args) {
-
-
-
-    }
-}
