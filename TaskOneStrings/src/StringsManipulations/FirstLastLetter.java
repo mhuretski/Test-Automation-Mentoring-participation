d0759ca334dd4fa42e36d728f7dc8f804c66b97a
Maksim Huretski <Maksim_Huretski@epam.com>
2018-07-31 10:49:50 +0300

    string builder

--- TaskOneStrings/src/StringsManipulations/FirstLastLetter.java
+++ TaskOneStrings/src/StringsManipulations/FirstLastLetter.java
@@ -18,12 +18,23 @@ public class FirstLastLetter {
         listOfStrings.add("Co an");
         System.out.println("Initial " + listOfStrings);
 
-        listOfStrings = replaceEachSecond(listOfStrings);
-        System.out.println("Replaced " + listOfStrings);
+        List<String> listOfStringsNew = new ArrayList<>(listOfStrings);
+        System.out.println("Copied " + listOfStringsNew);
 
+        long o1 = System.nanoTime();
+        listOfStrings = replace(listOfStrings);
+        long o2 = System.nanoTime();
+        System.out.println("Replaced with strings " + listOfStrings);
+
+        long i1 = System.nanoTime();
+        listOfStringsNew = replaceWithBuilder(listOfStringsNew);
+        long i2 = System.nanoTime();
+        System.out.println("Replaced with builder " + listOfStringsNew);
+
+        System.out.println("Strings " + (o2 - o1) + ". Builder " + (i2 - i1));
     }
 
-    private static List<String> replaceEachSecond(List<String> list) {
+    private static List<String> replace(List<String> list) {
         List<String> tempList = new ArrayList<>();
         char first;
         String tempWord;
@@ -39,6 +50,29 @@ public class FirstLastLetter {
                     first = list.get(i).charAt(0);
                     last = list.get(i).charAt(list.get(i).length() - 1);
                     tempWord = String.valueOf(last) + String.valueOf(first);
+                } else tempWord = list.get(i);
+            } else tempWord = list.get(i);
+            tempList.add(tempWord);
+        }
+        return tempList;
+    }
+
+    private static List<String> replaceWithBuilder(List<String> list) {
+        List<String> tempList = new ArrayList<>();
+        StringBuilder builder;
+        String tempWord;
+        for (int i = 0; i < list.size(); i++) {
+            if (i % 2 == 0 && i != 0) {
+                builder = new StringBuilder(list.get(i).length());
+                if (list.get(i).length() > 2) {
+                    builder.append(list.get(i).charAt(list.get(i).length() - 1))
+                            .append(list.get(i).substring(1, list.get(i).length() - 1))
+                            .append(list.get(i).charAt(0));
+                    tempWord = builder.toString();
+                } else if (list.get(i).length() == 2) {
+                    builder.append(list.get(i).charAt(list.get(i).length() - 1))
+                            .append(list.get(i).charAt(0));
+                    tempWord = builder.toString();
                 } else tempWord = list.get(i);
             } else tempWord = list.get(i);
             tempList.add(tempWord);
